d65330cf7eba9ff5fcb2e0c620e98bb3a685bc67
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-02-16 19:14:44 -0300

    Exercicio 2 incompleto

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/second-app/src/Content.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composicao de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composicao de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+];
+
+class Content extends React.Component{
+  render(){
+    return conteudos.map(({conteudo, bloco, status}) => `O conteúdo é: ${conteudo} Status: ${status} Bloco: ${bloco}`);        
+  }
+}
+
+export default Content;
